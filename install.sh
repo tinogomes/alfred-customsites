a305635fd5b280befd09cdf5ec922f30b01911c9
Celestino Gomes <tinorj@gmail.com>
2022-05-18 18:46:53 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ echo 'Killing Alfred'
 killall Alfred
 cd ~/Library/Application\ Support/Alfred
 mv customsites customsites_bkp
-git clone git://github.com/tinogomes/alfred-customsites.git customsites
+git clone git@github.com:tinogomes/alfred-customsites.git customsites
 echo 'Please, reopen Alfred'
